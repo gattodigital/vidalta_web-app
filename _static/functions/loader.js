9aaa297887fad01918e97a1885ac43a4fba17a30
Gatto Digital <contact@gattodigital.com>
2023-08-21 18:37:11 -0500

    base template and layout files with functionality

---
+++ _static/functions/loader.js
@@ -0,0 +1,48 @@
+async function loadContent(targetClass, contentPath) {
+    fetch(contentPath)
+        .then(response => response.text())
+        .then(data => {
+            const elements = document.querySelectorAll('.' + targetClass);
+            elements.forEach(element => {
+                element.innerHTML = data;  // Use innerHTML here
+            });
+        });
+}
+
+function determineTemplate() {
+    let templateName = 'home'; // default to home
+
+    if (window.location.hash === '#product-listing') {
+        templateName = 'product-listing';
+    } else if (window.location.hash === '#about-us') {
+        templateName = 'about';
+    } else if (window.location.hash === '#faqs') {
+        templateName = 'faqs';
+    } else if (window.location.hash === '#contact-us') {
+        templateName = 'contact';
+    } else if (window.location.hash === '#product-detail') {
+        templateName = 'product-detail';
+    } else if (window.location.hash === '#travel-guide') {
+        templateName = 'travel-guide';
+    } else if (window.location.hash === '#sitemap') {
+        templateName = 'sitemap';
+    } else if (window.location.hash === '#privacy-policy') {
+        templateName = 'privacy-policy';
+    } else if (window.location.hash === '#terms-and-conditions') {
+        templateName = 'terms-and-conditions';
+    } else if (window.location.hash === '#design-system') {
+        templateName = 'design-system';
+    }
+
+    loadContent('app-content', `templates/${templateName}.html`);
+}
+
+// Load header and footer
+loadContent('app-header', 'components/header/app-header.html');
+loadContent('app-footer', 'components/footer/app-footer.html');
+
+// Initial content load
+determineTemplate();
+
+// Add an event listener to handle URL hash changes
+window.addEventListener('hashchange', determineTemplate);
